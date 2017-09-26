df4a7553da0956b58366331725c5ad86a5fccc82
任文凯 <renwenkai@hujiang.com>
2017-09-26 09:21:50 +0800

    update: stream

--- Stream/demo1.js
+++ Stream/demo1.js
@@ -1,3 +1,14 @@
 // 书签（20170920) http://nodejs.cn/api/stream.html#stream_buffering
 
 // 书签（20170925）http://nodejs.cn/api/stream.html#stream_event_error
+
+const fs = require('fs');
+const file = fs.createWriteStream('./file.txt');
+
+for (let i = 0; i < 100; i++) {
+    file.write(`hello, #${i}!\n`);
+}
+
+file.end('This is the end\n', 'utf8', () => {
+    console.log('file write finished');
+});
