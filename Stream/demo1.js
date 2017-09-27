b3686d5d9cc8791a6e725b108e4725abdca9a0a6
任文凯 <renwenkai@hujiang.com>
2017-09-27 09:12:42 +0800

    update: stream

--- Stream/demo1.js
+++ Stream/demo1.js
@@ -3,12 +3,27 @@
 // 书签（20170925）http://nodejs.cn/api/stream.html#stream_event_error
 
 const fs = require('fs');
-const file = fs.createWriteStream('./file.txt');
+const path = require('path');
+const file = fs.createWriteStream(path.resolve(__dirname, './file.txt'));
 
+// file 是一个 wirtable 流
 for (let i = 0; i < 100; i++) {
     file.write(`hello, #${i}!\n`);
 }
 
-file.end('This is the end\n', 'utf8', () => {
+file.end('This is the end\n', () => {
     console.log('file write finished');
 });
+
+
+const file2 = fs.createWriteStream(path.resolve(__dirname, './file2.txt'));
+file2.cork();
+file2.write('some ');
+file2.cork();
+file2.write('data ');
+process.nextTick(() => {
+    file2.uncork();
+    file2.uncork();
+});
+
+//书签（20170927) http://nodejs.cn/api/stream.html#stream_writable_write_chunk_encoding_callback
